ac014e43243e80a86948068899a55010a26691d4
srubianof <43104565+srubianof@users.noreply.github.com>
2018-10-20 18:10:55 -0500

    Haciendo cursos iguales
    no sirve

--- laboratorio4/BLUEJ/excepcion/IemoisExcepcion.java
+++ laboratorio4/BLUEJ/excepcion/IemoisExcepcion.java
@@ -10,7 +10,7 @@ package excepcion;
 public class IemoisExcepcion extends Exception
 {
     public static final String  DISTRIBUIDOR= "El distribuidor falta";
-
+    public static final String  REPETIDO= "El curso ya se ha agregado";
     /**
      * Constructor for objects of class IemoisExcepcion
      */
