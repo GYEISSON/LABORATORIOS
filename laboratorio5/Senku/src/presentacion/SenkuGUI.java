78a9ebab50d815a2256db3397e2e1b114cc41b67
Santiago <santiago.rubiano@mail.escuelaing.edu.co>
2018-11-05 00:08:46 -0500

    Tablero
    Ya dibuja el tablero, falta crear la ficha, el panel para la ficha y la matriz para poder empezar a llenar el tablero de fichas

--- laboratorio5/Senku/src/presentacion/SenkuGUI.java
+++ laboratorio5/Senku/src/presentacion/SenkuGUI.java
@@ -7,6 +7,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 
+@SuppressWarnings("serial")
 public class SenkuGUI extends JFrame{
 	
 	private JMenuBar menuBar;
@@ -16,9 +17,10 @@ public class SenkuGUI extends JFrame{
 	private JMenuItem guardar;
 	private JMenuItem salir;
 
-
+	private tablero t;
 	public SenkuGUI(){
 		super("Senku"); 
+		t = new tablero();
 		prepareElementos();
 		prepareElementosMenu();
 		prepareAcciones();
@@ -48,6 +50,10 @@ public class SenkuGUI extends JFrame{
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		setSize(screenSize.width/2, screenSize.height/2);
 		setLocationRelativeTo(null);
+		
+		getContentPane().setLayout(new BorderLayout());
+        getContentPane().add(t,BorderLayout.CENTER);
+        t.repaint();
 	}
 	
 	private void prepareAcciones(){
@@ -113,5 +119,51 @@ public class SenkuGUI extends JFrame{
 		SenkuGUI se= new SenkuGUI();
 		se.setVisible(true);
 	}
+	
+	
+	
+class tablero extends JPanel{
+	
+	    public int TAMANO=40;
 
+	    public tablero() {
+	        setBackground(Color.white);
+	        setPreferredSize(new Dimension(200,200));       
+
+	    }
+
+
+	    public void paintComponent(Graphics g){
+	    	
+	    	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+			int x=(screenSize.width/2)/2;
+			int y=(screenSize.height/2)/2;
+			y=y-y/2;
+			x=x-x/3;
+
+	    	int largo,ancho;
+	    	largo=7;
+	    	ancho=3;
+	        super.paintComponent(g);
+	        //eje horizontal
+	        for (int f=0;f<=largo;f++){
+	            g.drawLine(f*TAMANO+x,80+y,f*TAMANO+x,ancho*TAMANO+80+y);
+	        }
+	        //eje vertical
+	        for (int cc=0;cc<=ancho;cc++){
+	            g.drawLine(0+x,cc*TAMANO+80+y,largo*TAMANO+0+x,cc*TAMANO+80+y);
+	        }
+	        
+	        
+	      //eje horizontal
+	        for (int f=0;f<=ancho;f++){
+	            g.drawLine(f*TAMANO+80+x,0+y,f*TAMANO+80+x,largo*TAMANO+y);
+	        }
+	        //eje vertical
+	        for (int cb=0;cb<=largo;cb++){
+	            g.drawLine(80+x,cb*TAMANO+y,ancho*TAMANO+80+x,cb*TAMANO+y);
+	        }
+	    }
+	}
+	        
 }
