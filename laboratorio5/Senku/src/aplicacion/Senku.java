56a82db5ae188461f384b5e0680e9e0ed60f2ae4
GYEISSON <yeison7f@gmail.com>
2018-11-05 23:07:09 -0500

    metodo de Senku mover desde una posicion a otra

--- laboratorio5/Senku/src/aplicacion/Senku.java
+++ laboratorio5/Senku/src/aplicacion/Senku.java
@@ -29,4 +29,26 @@ public class Senku {
 		}
 		matriz[hMatriz/2][wMatriz/2] = 1;
 	}
+
+	public void moveTo(int[] s,int[] t){
+		if(matriz[t[0]][t[1]] ==1 && matriz[s[0]][s[1]] ==2
+			&& ((s[0] == t[0] && (Math.abs(s[1]-t[1])==2)) 
+			|| (s[1] == t[1]  && (Math.abs(s[0]-t[0])==2)))) {
+			if(s[0] == t[0]) {
+				if((s[1]>t[1] && matriz[s[0]][s[1]-1]==2) || (s[1]<t[1] &&  matriz[s[0]][t[1]-1]==2)) {
+					matriz[s[0]][s[1]]=1;
+					matriz[t[0]][t[1]]=2;
+					if(s[1]>t[1]) matriz[s[0]][s[1]-1]=1;
+					else matriz[s[0]][t[1]-1]=1;
+				}
+			}else {
+				if((s[0]>t[0] && matriz[s[0]-1][s[1]]==2) || (s[0]<t[0] &&  matriz[t[0]-1][t[1]]==2)) {
+					matriz[s[0]][s[1]]=1;
+					matriz[t[0]][t[1]]=2;
+					if(s[0]>t[0]) matriz[s[0]-1][s[1]]=1;
+					else matriz[t[0]-1][t[1]]=1;	
+				}
+			}
+		}
+	}
 }
