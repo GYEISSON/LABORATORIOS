c159d817d1dde928978501475cbd8a2d35dd9ba3
srubianof <43104565+srubianof@users.noreply.github.com>
2018-11-06 18:36:22 -0500

    cambiando tamaños

---
+++ laboratorio5/src/presentacion/Ficha.java
@@ -0,0 +1,38 @@
+package presentacion;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.io.*;
+
+class Ficha extends JPanel {
+	private Color color;
+	private boolean visible;
+	private int value;
+	public Ficha(Color color,boolean visible,int value) {
+		super();
+		this.visible=visible;
+		this.color=color;
+		this.value=value;
+	} 
+	
+	
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		int h=this.getHeight();
+		g.setColor(color);
+		if(visible && (value==1)) {
+			g.fillOval(0, 0, h, h);
+			this.setBackground(Color.DARK_GRAY);
+		}
+		else if (!visible && (value==2)){
+			this.setBackground(Color.DARK_GRAY);
+		}
+		else {
+			this.setBackground(Color.BLACK);
+		}
+	}
+}
